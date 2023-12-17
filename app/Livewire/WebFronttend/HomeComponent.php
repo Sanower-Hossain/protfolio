f6714e530310bf9ce0fb1465cd946788a0d41f83
Sanower Hossain <sanower052@gmail.com>
2023-12-18 01:02:21 +0600

    init

---
+++ app/Livewire/WebFronttend/HomeComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\WebFronttend;
+
+use Livewire\Component;
+
+class HomeComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.web-fronttend.home-component');
+    }
+}
